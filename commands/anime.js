e9c955cbf6da0c00ee2012c7b1defb6ad3e5e155
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-05 01:10:28 +0530

    Fix - added command to nsfw category

--- commands/anime.js
+++ commands/anime.js
@@ -6,7 +6,7 @@ module.exports = {
         const anime = randomanime.anime();
         if(!message.guild) return;
         if(args.length > 1) return;
-      //  if(!message.channel.nsfw) return message.channel.send('oh no , this command will only work in NSFW marked channels :(')
+      if(!message.channel.nsfw) return message.channel.send('oh no , this command will only work in NSFW marked channels :(')
         const embed = new Discord.MessageEmbed()
         .setTitle('Random Anime Image')
         .setImage(anime)
