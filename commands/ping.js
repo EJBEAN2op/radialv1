8cfac59b8e29c714a2f6af91d3d83a84da5f411c
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-04 11:12:59 +0530

    Fix - Fixed ping showing NaN

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,13 @@
 module.exports = {
     name: 'ping',
-    execute: async(message , args) => {
-        const Discord = require('discord.js');
-const bot = new Discord.Client({disableMentions: 'everyone'});
-        if(message.author.bot) return;
+    execute(message, args , Discord , bot){
+        if(args.length > 1) return;
         var yourping = new Date().getTime() - message.createdTimestamp
-            var botping = Math.round(bot.ws.ping)
-            const embed = new Discord.MessageEmbed()
-                .setAuthor('ping calculator', 'https://cdn.discordapp.com/attachments/794680190553817108/795625238863544330/0EiFF0fjGhm13BvDS.gif')
-                .setColor('BLUE')
-                .setDescription(`**Your ping :** \n${yourping} \n**Bot's ping :** \n${botping}`)
-            message.reply(embed);
+                var botping = Math.round(bot.ws.ping)
+                const embed = new Discord.MessageEmbed()
+                    .setAuthor('ping calculator', 'https://cdn.discordapp.com/attachments/794680190553817108/795625238863544330/0EiFF0fjGhm13BvDS.gif')
+                    .setColor('BLUE')
+                    .setDescription(`**Your ping :** ${yourping} \n**Bot's ping :** ${botping}`)
+                message.channel.send(embed).catch(err => console.log(err));
     }
 }
