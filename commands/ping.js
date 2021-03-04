d73b9b7473f137c62c8e00ba2b7bb8ed84d51ec1
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-04 11:08:38 +0530

    Added per server prefix ( mongo atlas )

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,15 @@
 module.exports = {
     name: 'ping',
-    execute: async(message, bot , args) => {
+    execute: async(message , args) => {
+        const Discord = require('discord.js');
+const bot = new Discord.Client({disableMentions: 'everyone'});
         if(message.author.bot) return;
-        await message.reply('pong!')
+        var yourping = new Date().getTime() - message.createdTimestamp
+            var botping = Math.round(bot.ws.ping)
+            const embed = new Discord.MessageEmbed()
+                .setAuthor('ping calculator', 'https://cdn.discordapp.com/attachments/794680190553817108/795625238863544330/0EiFF0fjGhm13BvDS.gif')
+                .setColor('BLUE')
+                .setDescription(`**Your ping :** \n${yourping} \n**Bot's ping :** \n${botping}`)
+            message.reply(embed);
     }
 }
