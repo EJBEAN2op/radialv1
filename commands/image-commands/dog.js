cc1933f4d09cc9f3dfa7fc1465557cc367f0147c
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-06 22:28:35 +0530

    New - image commands

---
+++ commands/image-commands/dog.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'dog',
+    execute: async (message , args , Discord) => {
+        if(!message.guild) return;
+        if(args.length > 1) return;
+        const pets = require('pets.js')
+        const animal = 'dog'
+        let picture = await pets(animal)
+        message.channel.send(new Discord.MessageEmbed()
+        .setImage(picture))
+    }
+}
