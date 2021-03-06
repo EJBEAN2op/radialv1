60dbbd6d94fcdae05ca795475ee9acb945fb6241
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-06 23:15:14 +0530

    New - command cooldowns

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const {
     mongoPath
 } = require('./config.json')
 
-
+const cooldowns = new Discord.Collection();
 bot.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands')
 for (const folder of commandFolders) {
@@ -126,6 +126,27 @@ bot.on('message', async (message) => {
 
     const command = bot.commands.get(commandname) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandname));
     if (!command) return;
+
+    if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 0) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    
     try {
         command.execute(message, args, Discord, bot, prefix)
     } catch (error) {
