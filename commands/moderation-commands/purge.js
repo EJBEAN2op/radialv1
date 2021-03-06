c673743ac6073c89245bd44cb79d8c54064305ed
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-06 23:31:15 +0530

    Bug fix - credits <@485676072176713729>

--- commands/moderation-commands/purge.js
+++ commands/moderation-commands/purge.js
@@ -5,7 +5,8 @@ module.exports = {
         if (!message.guild) return;
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return;
         if (!message.guild.me.hasPermission('MANAGE_MESSAGES')) return;
-        
+        if (!args[1]) return message.channel.send('Please provide a valid number of messages for me to purge <:yeas:807926091540725791>')
+        if (isNaN(args[1])) return message.channel.send('Did you try using an actual number <:pfffft:807925840624877598>')
         if (parseInt(args[1]) > 100) return message.channel.send(`You cannot delete more than 100 messages in one go <:pepehands:817086292449034300>`)
         const num1 = +args[1]
         const num2 = 1
