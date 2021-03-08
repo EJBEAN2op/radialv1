ffdcae5b2cdc60db0317f1c3c6e001d00e20ee0b
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-08 18:31:27 +0530

    Bug fix : reason not showing in guild

--- commands/moderation-commands/ban.js
+++ commands/moderation-commands/ban.js
@@ -6,21 +6,21 @@ module.exports = {
             .setDescription('```diff\n- Error : Missing bot permission\n+ Permission Required : Ban Members```')).catch(console.error);
         if (!message.member.hasPermission('BAN_MEMBERS')) return;
         if (!args[1]) return message.channel.send(new Discord.MessageEmbed()
-            .setDescription('```css\nplease specify a user for me to ban\nUsage : !ban [user id / mention]```')).catch(console.error)
-        if (args.length > 3) return;
+            .setDescription('```diff\n- Error : missing arguments , please provide a user\n+ Usage : !ban [user id / mention]```')).catch(console.error)
         const target = message.guild.members.fetch(args[1].replace(/\D/g, ''));
         if (args[1].includes(message.author.id)) return message.channel.send('```diff\n- Error : you can\'t ban yourself').catch(console.error)
-        if ((await target).roles.highest.position >= message.guild.me.roles.highest.position) return message.channel.send(new Discord.MessageEmbed()
-            .setDescription(`\`\`\`diff\n- Error : ${(await target).user.username}'s role is higher than bot's role\`\`\``)).catch(console.error);
         target.then(user => {
+            if (user.roles.highest.position >= message.guild.me.roles.highest.position) return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`\`\`\`diff\n- Error : ${user.user.username}'s role is higher than bot's role\`\`\``)).catch(console.error);
+
             if (args[2]) {
                 user.send(new Discord.MessageEmbed()
-                    .setDescription(`\`\`\`diff\n+ You were banned in ${message.guild.name} \n+ Reason : ${args[2]}\`\`\``))
+                    .setDescription(`\`\`\`diff\n+ You were banned in ${message.guild.name} \n+ Reason : ${args.splice(2).join(" ")}\`\`\``))
                 user.ban({
-                    reason: `${args[2]}`
+                    reason: `${args.splice(2).join(" ")}`
                 }).catch(e => console.log(e));
                 message.channel.send(new Discord.MessageEmbed()
-                    .setDescription(`\`\`\`diff\n+ Banned user : ${user.user.tag} \n+ Reason : ${args[2]}\`\`\``)).catch(e => console.log(e));
+                    .setDescription(`\`\`\`diff\n+ Banned user : ${user.user.tag} \n+ Reason : ${args.splice(2).join(" ")}\`\`\``)).catch(e => console.log(e));
             } else {
                 user.send(new Discord.MessageEmbed()
                     .setDescription(`\`\`\`diff\n+ You were banned in ${message.guild.name} \n- Reason : Not provided`))
