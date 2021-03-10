e2126b9450643306906aa7679b74bf937439e89a
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-10 16:41:56 +0530

    antinuke commands

---
+++ commands/moderation-commands/antinuke-lock.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'lockdown',
+    execute(message, args, Discord) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return;
+        if (!message.guild.me.hasPermission('MANAGE_CHANNELS')) return message.channel.send(new Discord.MessageEmbed()
+            .setDescription('```diff\n- Error : Missing bot permission\n+ Permission Required : Manage channels```')).catch(console.error);
+        
+        message.guild.channels.cache.forEach(async (channel) => {
+            channel.updateOverwrite(message.guild.roles.everyone, {
+                SEND_MESSAGES: false
+            }).catch(console.error)
+        }).catch(console.error)
+        message.channel.send(new Discord.MessageEmbed()
+            .setDescription(`\`\`\`diff\n+ Locked ${message.channel.name}\`\`\``)
+            .setFooter('Use unlock to revert changes')).cache(console.error)
+    }
+}
---
+++ commands/moderation-commands/antinuke-unlock.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'unlock',
+    execute(message, args, Discord) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return;
+        if (!message.guild.me.hasPermission('MANAGE_CHANNELS')) return message.channel.send(new Discord.MessageEmbed()
+            .setDescription('```diff\n- Error : Missing bot permission\n+ Permission Required : Manage channels```')).catch(console.error);
+        message.guild.channels.cache.forEach(async (channel) => {
+            channel.updateOverwrite(message.guild.roles.everyone, {
+                SEND_MESSAGES: null
+            }).catch(console.error)
+        }).catch(console.error)
+        message.channel.send(new Discord.MessageEmbed()
+            .setDescription(`\`\`\`diff\n+ Locked ${message.channel.name}\`\`\``)
+            .setFooter('Use lock for a complete server lockdown')).cache(console.error)
+    }
+}
