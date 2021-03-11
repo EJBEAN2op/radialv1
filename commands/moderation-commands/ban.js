69041185e1e53afe41dd9c3f7deadf86d42a526e
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-11 17:45:46 +0530

    stlying fix

--- commands/moderation-commands/ban.js
+++ commands/moderation-commands/ban.js
@@ -27,7 +27,7 @@ module.exports = {
                     .setDescription(`\`\`\`diff\n+ You were banned in ${message.guild.name} \n- Reason : Not provided`))
                 user.ban().catch(e => console.log(e));
                 message.channel.send(new Discord.MessageEmbed()
-                    .setDescription(`\`\`\`diff\n+ Banned user : ${user.user.tag} \n- Reason : Not provided`)).catch(e => console.log(e));
+                    .setDescription(`\`\`\`diff\n+ Banned user : ${user.user.tag} \n- Reason : Not provided\`\`\``)).catch(e => console.log(e));
             }
         }).catch(e => message.channel.send(new Discord.MessageEmbed()
             .setDescription('```diff\n- Error : Unknown user / ID```'))).catch(console.error)
