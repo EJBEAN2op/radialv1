f7aa099c6fc51224bc85ee45d63ffc2e8d368cd9
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-13 16:09:24 +0530

    fix

--- commands/fun-commands/hack.js
+++ commands/fun-commands/hack.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'hack',
-    execute: async (message, args, Discord) => {
-        if (!message.guild) return;
+    execute: async (message , args , Discord) => {
+
+
+       
 
         if (!args[1]) return message.channel.send('yeah who are we hacking?')
 
@@ -14,73 +16,72 @@ module.exports = {
                 .setDescription('```diff\n- Error : unknown user / ID```'))
             return;
         }
-        hackuser.then(async user => {
-            const emails = ['smolpp@lmao.com', 'monkegang123@monke.com', 'needpussipls@gmail.lol', `${user.username}lolihunter@anime.gg`]
+        hackuser.then(user => {
+            const emails = ['smolpp@lmao.com' , 'monkegang123@monke.com' , 'needpussipls@gmail.lol' , `${user.username}lolihunter@anime.gg`]
             const emailnum = Math.floor(Math.random() * 3)
             const password = ['1234', 'milfhunter69', 'pussislayerxx', 'XxbigppmanxX', 'Mlgdorrito_xX', 'moNke123', 'password']
             const passwordnum = Math.floor(Math.random() * 7)
             const dm = ['yeah she lives there', 'man i have a small pp', 'send bobs', 'pls give kr pls', 'hey kitten join vc', 'jake is kinda cute']
             const dmnum = Math.floor(Math.random() * 6)
-            message.channel.send('Initiating totally dangerous hack')
-                .then(async (msg) => {
-                    setTimeout(async function () {
-                        await msg.edit('<a:loading:810404455609925653> Finding Discord Login... ');
-                    }, 3000)
-                    setTimeout(async function (){
-                        await msg.edit('Bypassing 2FA')
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Fetching account token');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`**Email :** \`${emails[emailnum]}\`\n**password :** \`${password[passwordnum]}\``);
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('<a:loading:810404455609925653> Installing Fortnite');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('<a:loading:810404455609925653> Hacking krunker account.. ');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Quick selling all skins <a:lolidance:819137344258572318>');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Searching google history');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('**Last search : how to lose virginity**');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`Searching for ${user.username}'s girlfriends`);
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Girlfriends count : 0 <:pepehands:817086292449034300>');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Finding last DM ...');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`last DM : ${dm[dmnum]}`);
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Fetching friends list ...');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit('Friends found : 0 <:pain:815180427257970689>');
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`Injecting trojan virus to the discriminator #${user.discriminator}`)
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`Reporting account for breaking discord TOS`)
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`Deleting cornhub account`)
-                    }, 3000)
-                    setTimeout(async function () {
-                        await msg.edit(`Successfully completed the totally real and dangerous hack`)
-                    }, 3000)
-                });
-        }).catch(e => message.channel.send('oh no something went wrong , contact EwasTaken#3961 to get me fixed'))
+
+        message.channel.send('Initiating totally dangerous hack').then(async a => {
+            await a.delete({timeout: 500})
+            message.channel.send('<a:loading:810404455609925653> Finding Discord Login... ').then(async b => {
+            await b.delete({timeout: 500})
+            message.channel.send('Bypassing 2FA').then(async c => {
+            await c.delete({timeout: 500})
+            message.channel.send('Fetching account token').then(async d => {
+            await d.delete({timeout: 500})
+            message.channel.send(`**Email :** \`${emails[emailnum]}\`\n**password :** \`${password[passwordnum]}\``).then(async e => {
+                await e.delete({timeout: 700})
+                message.channel.send('<a:loading:810404455609925653> Installing Fortnite').then(async f => {
+                    await f.delete({timeout: 500})
+                    message.channel.send('<a:loading:810404455609925653> Hacking krunker account.. ').then(async g => {
+                        await g.delete({timeout: 500})
+                        message.channel.send('Quick selling all skins <a:lolidance:819137344258572318>').then(async h => {
+                            await h.delete({timeout: 500})
+                            message.channel.send('Searching google history').then(async i => {
+                                await i.delete({timeout: 500})
+                                message.channel.send('**Last search : how to lose virginity**').then(async j => {
+                                    await j.delete({timeout: 500})
+                                    message.channel.send(`Searching for ${user.username}'s girlfriends`).then(async k => {
+                                        await k.delete({timeout: 500})
+                                        message.channel.send('Girlfriends count : 0 <:pepehands:817086292449034300>').then(async l => {
+                                            await l.delete({timeout: 500})
+                                            message.channel.send('Finding last DM ...').then(async m => {
+                                                await m.delete({timeout: 500})
+                                                message.channel.send(`last DM : ${dm[dmnum]}`).then(async n => {
+                                                    await n.delete({timeout: 500})
+                                                    message.channel.send('Fetching friends list ...').then(async o => {
+                                                        await o.delete({timeout: 500})
+                                                        message.channel.send('Friends found : 0 <:pain:815180427257970689>').then(async p => {
+                                                            await p.delete({timeout: 500})
+                                                            message.channel.send(`Injecting trojan virus to the discriminator #${user.discriminator}`).then(async q => {
+                                                                await q.delete({timeout: 500})
+                                                                message.channel.send(`Reporting account for breaking discord TOS`).then(async r => {
+                                                                    await r.delete({timeout: 500})
+                                                                    message.channel.send(`Deleting cornhub account`).then(async s => {
+                                                                        await s.delete({timeout: 500})
+                                                                        message.channel.send(`Successfully completed the totally real and dangerous hack`)
+                                                                        })
+                                                                    })
+                                                                })
+                                                            })
+                                                        })
+                                                    })
+                                                })
+                                            })
+                                        })
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+            })
+            })
+        })
+    }).catch(e => message.channel.send('oh no something went wrong , contact EwasTaken#3961 to get me fixed')).catch(console.error)
     }
 }
