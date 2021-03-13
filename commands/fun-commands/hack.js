a336290bff71797e3b609d2766198663b76b4ad5
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-13 16:18:11 +0530

    Add files via upload

--- commands/fun-commands/hack.js
+++ commands/fun-commands/hack.js
@@ -25,43 +25,43 @@ module.exports = {
             const dmnum = Math.floor(Math.random() * 6)
 
         message.channel.send('Initiating totally dangerous hack').then(async a => {
-            await a.delete({timeout: 500})
+            await a.delete({timeout: 2000})
             message.channel.send('<a:loading:810404455609925653> Finding Discord Login... ').then(async b => {
-            await b.delete({timeout: 500})
+            await b.delete({timeout: 2000})
             message.channel.send('Bypassing 2FA').then(async c => {
-            await c.delete({timeout: 500})
+            await c.delete({timeout: 2000})
             message.channel.send('Fetching account token').then(async d => {
-            await d.delete({timeout: 500})
+            await d.delete({timeout: 2000})
             message.channel.send(`**Email :** \`${emails[emailnum]}\`\n**password :** \`${password[passwordnum]}\``).then(async e => {
                 await e.delete({timeout: 700})
                 message.channel.send('<a:loading:810404455609925653> Installing Fortnite').then(async f => {
-                    await f.delete({timeout: 500})
+                    await f.delete({timeout: 2000})
                     message.channel.send('<a:loading:810404455609925653> Hacking krunker account.. ').then(async g => {
-                        await g.delete({timeout: 500})
+                        await g.delete({timeout: 2000})
                         message.channel.send('Quick selling all skins <a:lolidance:819137344258572318>').then(async h => {
-                            await h.delete({timeout: 500})
+                            await h.delete({timeout: 2000})
                             message.channel.send('Searching google history').then(async i => {
-                                await i.delete({timeout: 500})
+                                await i.delete({timeout: 2000})
                                 message.channel.send('**Last search : how to lose virginity**').then(async j => {
-                                    await j.delete({timeout: 500})
+                                    await j.delete({timeout: 2000})
                                     message.channel.send(`Searching for ${user.username}'s girlfriends`).then(async k => {
-                                        await k.delete({timeout: 500})
+                                        await k.delete({timeout: 2000})
                                         message.channel.send('Girlfriends count : 0 <:pepehands:817086292449034300>').then(async l => {
-                                            await l.delete({timeout: 500})
+                                            await l.delete({timeout: 2000})
                                             message.channel.send('Finding last DM ...').then(async m => {
-                                                await m.delete({timeout: 500})
+                                                await m.delete({timeout: 2000})
                                                 message.channel.send(`last DM : ${dm[dmnum]}`).then(async n => {
-                                                    await n.delete({timeout: 500})
+                                                    await n.delete({timeout: 2000})
                                                     message.channel.send('Fetching friends list ...').then(async o => {
-                                                        await o.delete({timeout: 500})
+                                                        await o.delete({timeout: 2000})
                                                         message.channel.send('Friends found : 0 <:pain:815180427257970689>').then(async p => {
-                                                            await p.delete({timeout: 500})
+                                                            await p.delete({timeout: 2000})
                                                             message.channel.send(`Injecting trojan virus to the discriminator #${user.discriminator}`).then(async q => {
-                                                                await q.delete({timeout: 500})
+                                                                await q.delete({timeout: 2000})
                                                                 message.channel.send(`Reporting account for breaking discord TOS`).then(async r => {
-                                                                    await r.delete({timeout: 500})
+                                                                    await r.delete({timeout: 2000})
                                                                     message.channel.send(`Deleting cornhub account`).then(async s => {
-                                                                        await s.delete({timeout: 500})
+                                                                        await s.delete({timeout: 2000})
                                                                         message.channel.send(`Successfully completed the totally real and dangerous hack`)
                                                                         })
                                                                     })
