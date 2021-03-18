5ba3108ee5e5674e8cf666003319e11ee704efc1
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-19 01:50:40 +0530

    error handling

--- commands/fun-commands/avatar.js
+++ commands/fun-commands/avatar.js
@@ -1,19 +1,32 @@
+const errorText = require('../../embeds/text')
 module.exports = {
     name: 'av',
-    execute(message, args, Discord, bot) {
-
+    execute: async (message, args, Discord, bot) => {
         if (args.length > 2) return;
-
-        //message.mentions.members.first()
-        if (!args[1]) return message.channel.send(new Discord.MessageEmbed()
-            .setAuthor(`${message.author.tag}`)
-            .setTitle('Avatar')
-            .setImage(message.author.displayAvatarURL({
-                dynamic: true,
-                size: 2048
-            })));
-
+        if (!args[1]) {
+            try {
+                await message.channel.send(new Discord.MessageEmbed()
+                    .setAuthor(`${message.author.tag}`)
+                    .setTitle('Avatar')
+                    .setImage(message.author.displayAvatarURL({
+                        dynamic: true,
+                        size: 2048
+                    })));
+                    return;
+            } catch (err) {
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription(`\`\`\`diff\n- Error : ${err}\`\`\`\n ${errorText}`)).catch(console.error);
+                    return;
+            }
+        }
         const avUser = message.client.users.fetch(args[1].replace(/\D/g, ''));
+        try {
+            await avUser
+        } catch (e) {
+            message.channel.send(new Discord.MessageEmbed()
+                .setDescription('```diff\n- Error : unknown user / ID```')).catch(console.error);
+            return;
+        }
         avUser.then(user => {
             message.channel.send(new Discord.MessageEmbed()
                 .setAuthor(`${user.tag}`)
@@ -21,10 +34,9 @@ module.exports = {
                 .setImage(user.displayAvatarURL({
                     dynamic: true,
                     size: 2048
-                })));
+                }))).catch(console.error);
         }).catch(err => message.channel.send(new Discord.MessageEmbed()
-            .setColor('RED')
-            .setDescription(`<:exclala:812387008365985804> User not found!`))).catch(err => console.log(err));
+            .setDescription(`\`\`\`diff\n- Error : ${err}\`\`\`\n ${errorText}`))).catch(console.error);
 
     }
 }
