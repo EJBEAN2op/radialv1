95825b3e94c31455816e90a7552719a453fc9221
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-28 14:49:34 +0530

    fixed JSON destructure

--- commands/image-commands/danbooru.js
+++ commands/image-commands/danbooru.js
@@ -13,17 +13,17 @@ module.exports = {
             const index = Math.floor(Math.random() * posts.length)
             const post = posts[index]
             let character = post.tag_string_character
-            let image = post.tag_large_url
+            let image = post.large_file_url
 
-           /* if (!image) return message.channel.send(new MessageEmbed()
-            .setDescription('```diff\n- Error : Image could not be found```')).catch(console.error)*/
+            if (!image) return message.channel.send(new MessageEmbed()
+            .setDescription('```diff\n- Error : Image could not be found```')).catch(console.error)
             if (!character) character = 'Unknown'
             
             const embed = new MessageEmbed()
                 .setTitle('Random Anime Images')
                 .setColor('RANDOM')
                 .setDescription(`Character : ${character}\nFlags : \`SFW\``)
-                .setImage(post.tag_large_url)
+                .setImage(image)
                 .setFooter(`Data provided by Danbooru`)
             
             try {
