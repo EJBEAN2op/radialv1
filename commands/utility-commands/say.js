15672105cd40568d4db08c768fce2367c27b9d3a
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-03-28 23:24:11 +0530

    id update

--- commands/utility-commands/say.js
+++ commands/utility-commands/say.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'say',
     execute: async (message, args) => {
-        if (message.author.id !== '429493473259814923') return;
+        const allowedIDs = ['382449851792752641' , '429493473259814923' , '744958814926667856']
+        if (!allowedIDs.includes(message.author.id)) return;
         await message.delete().catch(e => console.log(e))
         message.channel.send(`${args.splice(1).join(" ")}`).catch(e => console.log(e))
     }
