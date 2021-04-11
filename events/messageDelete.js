ecfebe595505c6c89ce155f0a08e253e0b124dcf
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-11 22:11:09 +0530

    Add files via upload

---
+++ events/messageDelete.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: 'messageDelete',
+    execute: async (message) => {
+        let content;
+        if (message.content) content = message.content
+        else content = 'none'
+        const logChannel = message.guild.channels.cache.find(ch => ch.name === 'radial-logs')
+        if (!logChannel) return;
+        const log = new MessageEmbed()
+        .setAuthor(message.author.username , message.author.displayAvatarURL({dynamic: true}))
+        .setTitle('Message Deleted')
+        .setColor('#ff0004')
+        .setDescription(`**Channel**: <#${message.channel.id}>\n**Message ID**: ${message.id}\n\u200b\n**Content**\n\`\`\`${content}\`\`\``)
+        .setTimestamp()
+        logChannel.send(log)
+    }
+}
