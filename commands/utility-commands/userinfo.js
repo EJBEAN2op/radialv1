a1907d604816494d9e2c8875ce079a8679467e8a
EJBEAN <64283902+EJBEAN2op@users.noreply.github.com>
2021-04-12 14:11:33 +0530

    account integration

--- commands/utility-commands/userinfo.js
+++ commands/utility-commands/userinfo.js
@@ -1,14 +1,20 @@
+const emotes = require('../../JSON/emotes.json')
+const accountSchema = require('../../schemas/account-schema')
 module.exports = {
     name: 'userinfo',
     aliases: ['whois', 'user'],
-    execute(message , args , Discord ){
+    execute(message , args , Discord , bot , prefix , password , signedInUser){
         if(args.length > 2) return;
         if(!message.guild) return;
+        let signedin;
+        let singedinmember;
+        if (message.author.id === signedInUser) signedin = 'Premium User'
+        else signedin = 'Standard User'
         const infoembed = new Discord.MessageEmbed()
                 .setTitle('User info')
                 .setDescription(`<@${message.author.id}>`)
                 .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-                .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setAuthor(`${message.author.tag} [${signedin}]`, message.author.displayAvatarURL({ dynamic: true }))
                 .addField('Username:', `${message.author.username}`, true)
                 .addField('Discriminator:', `${message.author.discriminator}`, true)
                 .addField('ID:', `\`${message.author.id}\``, true)
@@ -25,12 +31,15 @@ module.exports = {
 
 
                 const infoUser = message.guild.members.fetch(args[1].replace(/\D/g, ''));
-                infoUser.then(user => {
+                infoUser.then(async user => {
+                    const data = await accountSchema.findOne({userID: user.user.id})
+                    if (data) singedinmember = 'Premium User'
+                    else singedinmember = 'Standard User'
             const infoembed2 = new Discord.MessageEmbed()
                 .setTitle('User Info')
                 .setDescription(`<@${user.user.id}>`)
                 .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
-                .setAuthor(`${user.user.tag}`, user.user.displayAvatarURL({ dynamic: true }))
+                .setAuthor(`${user.user.tag} [${singedinmember}]`, user.user.displayAvatarURL({ dynamic: true }))
                 .addField('Username:', `${user.user.username}`, true)
                 .addField('Discriminator:', `${user.user.discriminator}`, true)
                 .addField('ID:', `\`${user.user.id}\``, true)
@@ -44,6 +53,6 @@ module.exports = {
 
              message.channel.send(infoembed2)}).catch(err => message.channel.send(new Discord.MessageEmbed()
 .setColor('RED')
-.setDescription(`<:exclala:812387008365985804> User not found!`))).catch(err => console.log(err));
+.setDescription(`${emotes.emotes.alert} User not found!`))).catch(err => console.log(err));
     }
 }
